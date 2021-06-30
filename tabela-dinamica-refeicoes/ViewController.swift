d33bb7743dd555ca9466273e96d5f0c8fb7ec36a
d-llirium <plimacarlos@gmail.com>
2021-06-30 13:27:14 -0300

    make a dynamic table view

--- tabela-dinamica-refeicoes/ViewController.swift
+++ tabela-dinamica-refeicoes/ViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+//ViewController implementa a classe de UITableViewController
+class ViewController: UITableViewController {
+    //criando uma lista para as refeições
+    let refeicoes = ["Churros","Macarrão","Pizza"]
+    //de UITableViewController
     override func viewDidLoad() {
+        //super p herdar da classe mãe
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("tableViewController foi carregada")
     }
-
-
+    
+    //do protocolo UITableViewDataSource de dentro do UITableViewController
+    //para atribuir quantidade de linhas na tabela
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //retorna a quantidade de itens da array como quantidades de linhas
+        return refeicoes.count
+    }
+    //para retornar o conetúdo das células
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+        //pega cada um os elementos da lista que está sendo renderizado
+        let refeicao = refeicoes[indexPath.row]
+        //transforma o elemento da lista em uma linha de texto
+        celula.textLabel?.text = refeicao
+        return celula
+    }
 }
 
